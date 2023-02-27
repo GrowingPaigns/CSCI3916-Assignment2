d3b7995b770a3396dcb6e27a3d48a0c679093a8b
GrowingPaigns <samhilfer70@gmail.com>
2023-02-26 19:21:49 -0700

    Close to finishing assignment

--- server.js
+++ server.js
@@ -23,47 +23,91 @@ app.use(passport.initialize());
 
 var router = express.Router(); // for handling git requests
 
+
 function getJSONObjectForMovieRequirement(req) { // takes in the request obj
     var json = {
-        headers: "No headers",
+        headers: "[No Query Headers]",
         key: process.env.UNIQUE_KEY,
-        body: "No body"
+        body: "[Empty Query Body]",
+        parameters: "[No Query Parameters]"
 
     };
 
-    /* checks above json values - takes care of the first part of "If the server
-     * accepts a request..."
+    /* Checks for/assigns values to above json variables
+     * [NOTE: Object.keys(___) gathers the keys from each k:v pair in req;
+     * .length arg means we are gathering the number of keys in req]
      */
-    if (req.body != null) {
+
+    if (Object.keys(req.body).length >= 1) {
         json.body = req.body;
     }
-    if (req.headers != null) {
+    if (Object.keys(req.headers).length >= 1) {
         json.headers = req.headers;
+    }
+    if (Object.keys(req.query).length >= 1) {
+        json.parameters = req.query;
+
     }
 
     return json;
 }
 
+const signInUpMethods = ['POST']
+const movieMethods = ['POST','DELETE','PUT','GET']
+function methodCheck(req, res, next){
+    const method = req.method.toUpperCase();
+    if (!signInUpMethods.includes(method)){
+        return res.status(405).send('Unsupported HTTP Method [' + method + '] Input');
+    }else{
+        next();
+    }
+
+}
+function movieMethodCheck(req, res, next){
+    const method = req.method.toUpperCase();
+    if (!movieMethods.includes(method)){
+        return res.status(405).send('Unsupported HTTP Method [' + method + '] Input');
+    }else{
+        next();
+    }
+
+}
+// check if call to base URL was made
+router.all('/', (req, res) => {
+    if (req.originalUrl === '/'){
+        res.status(401).send({success: false, msg: '[ERROR - Tried to Access Base URL]'});
+        return;
+    }
+});
+
+/* Method Check: ensure that no improper methods were used in sign in/up process */
+router.route(['/signup', '/signin'])
+    .all(methodCheck) // does not account for HEAD or OPTIONS
+
 /* Server Route #1: Signing Up */
 // connect to router which is listening for a post on the 'signup' path
 // set up anon function which takes request/response (ONLY LISTENS FOR 'POST')
 router.post('/signup', (req, res) => { // takes in json object body
+
     if (!req.body.username || !req.body.password) { // error check for id/password
-        res.json({success: false, msg: 'Please include both username and password to signup.'})
+        res.json({success: false, message: 'Please include both username and password to signup.'})
     } else {
         var newUser = { // creates a new user with the received variables
             username: req.body.username,
             password: req.body.password
         };
+        var o = getJSONObjectForMovieRequirement(req);
 
         db.save(newUser); //no duplicate checking - just appending/saving to the array
-        res.json({success: true, msg: 'Successfully created new user.'}) // return success msg
+        res.json({success: true, message: 'Successfully created new user.',response: o}) // return success msg
+
+        //res.json(o); // send json back to the object
     }
 });
 
-
 /* Server Route #2: Signing in */
 router.post('/signin', (req, res) => {
+
     var user = db.findOne(req.body.username); // gathers user from db
 
     if (!user) { // 401 ERROR check
@@ -78,7 +122,10 @@ router.post('/signin', (req, res) => {
             // used to authenticate key from env file
             var token = jwt.sign(userToken, process.env.SECRET_KEY);
             // if successful send jwt token
-            res.json ({success: true, token: 'JWT ' + token});
+            var o = getJSONObjectForMovieRequirement(req);
+            res.json ({success: true, token: 'JWT ' + token , response: o});
+
+            //res.json(o); // send json back to the object
         }
         else { // if passwords do no match - send error
             res.status(401).send({success: false, msg: 'Authentication failed.'});
@@ -86,13 +133,36 @@ router.post('/signin', (req, res) => {
     }
 });
 
+/* Server Route #3: Movie Collection */
 router.route('/testcollection') // this will be movie
-    //.post
-    //.get
+    .all(movieMethodCheck) // ensure no improper methods are used in movie calls
+    .post((req, res) => {
+            console.log(req.body);
+            res = res.status(200);
+            // if request has a content type, set response to same type
+            if (req.get('Content-Type')) {
+                res = res.type(req.get('Content-Type'));
+            }
+            var o = getJSONObjectForMovieRequirement(req);
+            res.json(o); // send json back to the object
+        }
+    )
+    .get((req, res) => {
+            console.log(req.body);
+            res = res.status(200);
+            // if request has a content type, set response to same type
+            if (req.get('Content-Type')) {
+                res = res.type(req.get('Content-Type'));
+            }
+            var o = getJSONObjectForMovieRequirement(req);
+            res.json(o); // send json back to the object
+        }
+    )
     .delete(authController.isAuthenticated, (req, res) => {
             console.log(req.body);
             res = res.status(200);
-            if (req.get('Content-Type')) { // if request has a content type, set response to same type
+            // if request has a content type, set response to same type
+            if (req.get('Content-Type')) {
                 res = res.type(req.get('Content-Type'));
             }
             var o = getJSONObjectForMovieRequirement(req);
@@ -100,6 +170,7 @@ router.route('/testcollection') // this will be movie
         }
     )
     .put(authJwtController.isAuthenticated, (req, res) => {
+
             console.log(req.body);
             res = res.status(200);
             if (req.get('Content-Type')) {
@@ -111,10 +182,8 @@ router.route('/testcollection') // this will be movie
     );
 
 
-/* TODO
-*  Create route to handle GET and POST movie methods
-*       route above handles authentication of PUT/DELETE movie methods
-* */
+
+
 app.use('/', router); // app.use on root
 app.listen(process.env.PORT || 1818); // port taken from render/local machine
 module.exports = app; // for unit testing - without export, we cant leverage the file
